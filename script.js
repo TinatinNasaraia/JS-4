1f7f6de4ee58866e06f32e75daae12346c286b51
TinatinNasaraia <tikanasaraia@gmail.com>
2022-09-10 18:10:25 +0400

    JS-4

---
+++ script.js
@@ -0,0 +1,20 @@
+//1.
+
+//let array= ['academy', 'of', 'digital', 'industries'].reduce(function(x, y) {
+    //return x +" ".concat(y);
+//});
+//console.log(array);
+
+
+
+//2.
+
+//let array =[23,45,32,5,87,7,3,98].sort((x,y)=> y-x);
+
+//console.log(array);
+
+
+//let array1 =[23,45,32,5,87,7,3,98]
+
+//let minvalue = array1.sort((x, y) => y - x)[array1.length-1];
+//console.log(minvalue);
